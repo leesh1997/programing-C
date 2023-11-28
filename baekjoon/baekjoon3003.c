09e44b92a489a882aa2bb5f525e0f8c705857a03
이승훈 <95062919+leesh1997@users.noreply.github.com>
2023-11-28 20:44:09 +0900

    baekjoon3003
    baekjoon3003

---
+++ baekjoon/baekjoon3003.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int	a, b, c, d, e, f;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+	scanf("%d", &d);
+	scanf("%d", &e);
+	scanf("%d", &f);
+
+	printf("%d ", 1 - a);
+	printf("%d ", 1 - b);
+	printf("%d ", 2 - c);
+	printf("%d ", 2 - d);
+	printf("%d ", 2 - e);
+	printf("%d ", 8 - f);
+}
